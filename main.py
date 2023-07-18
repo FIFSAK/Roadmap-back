68af3703ab8f59f39f5fd4354cfe48096ffde048
FIFSAK <anuar200572@gmail.com>
2023-07-18 12:02:22 +0600

    survey works

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ async def receive_answers(answers: Answers):
                 40,
             )
     print(response)
-    return response
+    return {"message": response}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, workers=3)
